e77b3ede826efaca38bffddb713b4bbd706f7bbc
Katana Tran <tkatyk21@gmail.com>
2019-03-23 20:54:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "   |   |   "
-  11.times{puts("-")}
+  11.times{print("-")}
   puts "   |   |   "
   11.times{print("-")}
   puts "   |   |   "
